d6ca18be54f9be342be070fa33163eb01b5c4e3a
Iryna-Matiash <165329434+Iryna-Matiash@users.noreply.github.com>
2025-05-11 17:40:57 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,35 @@
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
-//import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/contactsOps";
+import Loader from "../Loader/Loader";
+import Error from "../Error/Error";
+import {
+  selectContacts,
+  selectLoading,
+  selectError,
+} from "../../redux/contactsSlice";
 
 export default function App() {
-  // const [data, setData] = useState(() => {
-  //   const savedData = JSON.parse(window.localStorage.getItem("savedData"));
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
 
-  //   return savedData ? savedData : İnitialData;
-  // });
-
-  //const [filter, setFilter] = useState("");
-
-  // useEffect(() => {
-  //   window.localStorage.setItem("savedData", JSON.stringify(data));
-  // }, [data]);
-
-  // const addData = (values) => {
-  //   const newData = {
-  //     name: values.name,
-  //     number: values.number,
-  //     id: nanoid(),
-  //   };
-
-  //   setData((prevData) => {
-  //     return [...prevData, newData];
-  //   });
-  // };
-
-  // const deleteData = (dataİd) => {
-  //   setData((prevData) => {
-  //     return prevData.filter((data) => data.id !== dataİd);
-  //   });
-  // };
-
-  // const filteredData = data.filter((el) =>
-  //   el.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-  // );
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      {isLoading && <Loader>Loading ...</Loader>}
+      {isError && <Error>Something went wrong</Error>}
+      {contacts.length > 0 && <ContactList />}
     </div>
   );
 }
